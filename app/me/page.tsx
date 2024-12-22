d1b8dd31a91499de884647092100715254250203
Ayush Sagar <saggar.ayush@gmail.com>
2024-12-22 10:18:45 +0530

    spinner

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -2,13 +2,56 @@
 
 import { useEffect, useState } from "react"
 import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Progress } from "@/components/ui/progress"
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { CalendarIcon, InfoIcon, MoreVertical } from "lucide-react"
 import { FaPlaneDeparture } from "react-icons/fa"
 import { Bar, BarChart, CartesianGrid, XAxis } from "recharts"
 import { ChartConfig, ChartContainer } from "@/components/ui/chart"
+import { TrendingUp } from "lucide-react"
+import { Label, Pie, PieChart } from "recharts"
+
+import {
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+import React from "react"
+
+
+const pieData = [
+  { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
+  { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
+  { browser: "firefox", visitors: 287, fill: "var(--color-firefox)" },
+  { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
+  { browser: "other", visitors: 190, fill: "var(--color-other)" },
+]
+
+const pieConfig = {
+  visitors: {
+    label: "Visitors",
+  },
+  chrome: {
+    label: "Chrome",
+    color: "hsl(var(--chart-1))",
+  },
+  safari: {
+    label: "Safari",
+    color: "hsl(var(--chart-2))",
+  },
+  firefox: {
+    label: "Firefox",
+    color: "hsl(var(--chart-3))",
+  },
+  edge: {
+    label: "Edge",
+    color: "hsl(var(--chart-4))",
+  },
+  other: {
+    label: "Other",
+    color: "hsl(var(--chart-5))",
+  },
+} satisfies ChartConfig
 
 const chartConfig = {
   desktop: {
@@ -34,6 +77,10 @@ const chartData = [
 
 export default function LeavePage() {
   const [randomHeights, setRandomHeights] = useState<number[]>([])
+  const totalVisitors = React.useMemo(() => {
+    return pieData.reduce((acc, curr) => acc + curr.visitors, 0)
+  }, [])
+
 
   useEffect(() => {
     const heights = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'].map(
@@ -160,21 +207,68 @@ export default function LeavePage() {
                   </div>
                 </CardContent>
               </Card>
-              <Card className="h-[20rem] w-[15rem]">
-                <CardHeader>
-                  <CardTitle className="flex items-center text-sm font-medium">
-                    Consumed Leave Types
-                    <InfoIcon className="ml-2 h-4 w-4" />
-                  </CardTitle>
-                </CardHeader>
-                <CardContent>
-                  <div className="relative aspect-square">
-                    <div className="absolute inset-0 flex items-center justify-center">
-                      <span className="text-sm font-medium">Leave Types</span>
-                    </div>
-                  </div>
-                </CardContent>
-              </Card>
+              <Card className="flex flex-col">
+      <CardHeader className="items-center pb-0">
+      </CardHeader>
+      <CardContent className="flex-1 pb-0">
+        <ChartContainer
+          config={pieConfig}
+          className="mx-auto aspect-square max-h-[250px]"
+        >
+          <PieChart>
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Pie
+              data={pieData}
+              dataKey="visitors"
+              nameKey="browser"
+              innerRadius={60}
+              strokeWidth={5}
+            >
+              <Label
+                content={({ viewBox }) => {
+                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                    return (
+                      <text
+                        x={viewBox.cx}
+                        y={viewBox.cy}
+                        textAnchor="middle"
+                        dominantBaseline="middle"
+                      >
+                        <tspan
+                          x={viewBox.cx}
+                          y={viewBox.cy}
+                          className="fill-foreground text-3xl font-bold"
+                        >
+                          {totalVisitors.toLocaleString()}
+                        </tspan>
+                        <tspan
+                          x={viewBox.cx}
+                          y={(viewBox.cy || 0) + 24}
+                          className="fill-muted-foreground"
+                        >
+                          Visitors
+                        </tspan>
+                      </text>
+                    )
+                  }
+                }}
+              />
+            </Pie>
+          </PieChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="flex-col gap-2 text-sm">
+        <div className="flex items-center gap-2 font-medium leading-none">
+          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+        </div>
+        <div className="leading-none text-muted-foreground">
+          Showing total visitors for the last 6 months
+        </div>
+      </CardFooter>
+    </Card>
               <Card className="w-[44rem]">
                 <CardHeader>
                   <CardTitle className="flex items-center text-sm font-medium">
